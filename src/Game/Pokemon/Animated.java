1426ea663566edc61a1c1f5869370afaef328401
Nicholas Aso <aso.nicholas@gmail.com>
2016-10-12 14:23:16 -0700

    This commit has re-envisioned methods but crashes due to inflateException

--- src/Game/Pokemon/Animated.java
+++ src/Game/Pokemon/Animated.java
@@ -9,8 +9,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import java.io.InputStream;
 
 public class Animated extends View {
@@ -24,19 +27,19 @@ public class Animated extends View {
             USER_FRAME_TOPRIGHT_X, USER_FRAME_TOPRIGHT_Y;
     protected static String pokemon;
     protected static int shift_x = 0, shift_x2 = 0, shift_y = 0, shift_y2 = 0;
-    protected int LARGEST_HEIGHT = 181, LARGEST_WIDTH = 217;
+    protected int LARGEST_HEIGHT = 0, LARGEST_WIDTH = 0;
     private final float PLACEMENT_X = 1.25f, PLACEMENT_X2 = 0.5f, 
             PLACEMENT_Y = 1.0f, PLACEMENT_Y2 = 1.0f;
-    private AssetManager assets;
+    private AssetManager asset;
     private Battle battle;
     private final Paint brush = new Paint();
-    private int contender, height, time, width, x, y;
+    private int contender, height, time, width, x, y, FIXED_FRAME;
     private boolean draw;
-    private String[] files, folders;
-    private Bitmap frame, frame2;
+    private String[] frames, files, folders;
+    private Bitmap opponent, user;
     private Integer i, j;
-    private Options options, options2;
-    private String orientation, temp;
+    private Options Opponent_Options, User_Options;
+    private String orientation, temp, Largest_Width_Details, Largest_Height_Details;
     private InputStream stream;
 
     public Animated(Context context){
@@ -58,120 +61,115 @@ public class Animated extends View {
     }
 
     private void initialize(Context context){
-        assets = context.getAssets();
+//        WindowManager window = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+//        Display screen = window.getDefaultDisplay();
+//        Point size = new Point();
+//        screen.getSize(size);
+//        FIXED_FRAME = Math.min(size.x, size.y);
+//        Battle.text.setText("Width = " + size.x + " | Height = " + size.y);
+        asset = context.getAssets();
+//        setLargest();
         setPokemon("bulbasaur");
         i = 0;
         j = 0;
-        time = 10;
+        time = 15;
         draw = true;
         HEALTH_BAR_SIZE = LARGEST_WIDTH*1.5f;
         
         try {
             orientation = "front";
             contender = 1;
-            stream = assets.open("sprites/" + pokemon + "/" + orientation + "/frame_" + i.toString() + ".png");
+            stream = asset.open("sprites/" + pokemon + "/" + orientation + "/frame_" + i + ".png");
             decodeResizedBitmapFromAssets(LARGEST_WIDTH, LARGEST_HEIGHT);
             
             orientation = "back";
             contender = 2;
-            stream = assets.open("sprites/" + pokemon + "/" + orientation + "/frame_" + j.toString() + ".png");
+            stream = asset.open("sprites/" + pokemon + "/" + orientation + "/frame_" + j + ".png");
             decodeResizedBitmapFromAssets(LARGEST_WIDTH, LARGEST_HEIGHT);
-        } catch (Exception e) {
-            try {
-                orientation = "front";
-                contender = 1;
-                stream = assets.open("sprites/" + pokemon + "/" + orientation + "/frame_" + i.toString() + ".png");
-                decodeResizedBitmapFromAssets(LARGEST_WIDTH, LARGEST_HEIGHT);
-                
-                orientation = "back";
-                contender = 2;
-                stream = assets.open("sprites/" + pokemon + "/" + orientation + "/frame_" + j.toString() + ".png");
-                decodeResizedBitmapFromAssets(LARGEST_WIDTH, LARGEST_HEIGHT);
-            } catch (Exception e2) {
-            }
+        } catch (Exception e) {      
         }
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
         
-        OPPONENT_FRAME_TOPLEFT_X = 1.25f*(2*canvas.getWidth()/3.0f - options2.outWidth/2.0f) + shift_x;
-        OPPONENT_FRAME_TOPRIGHT_X = OPPONENT_FRAME_TOPLEFT_X + frame.getWidth();
-        OPPONENT_FRAME_TOPLEFT_Y = 1.0f*(9*canvas.getHeight()/20.0f + LARGEST_HEIGHT/2.0f - options2.outHeight);
+        OPPONENT_FRAME_TOPLEFT_X = 1.25f*(2*canvas.getWidth()/3.0f - User_Options.outWidth/2.0f) + shift_x;
+        OPPONENT_FRAME_TOPRIGHT_X = OPPONENT_FRAME_TOPLEFT_X + opponent.getWidth();
+        OPPONENT_FRAME_TOPLEFT_Y = 1.0f*(9*canvas.getHeight()/20.0f + LARGEST_HEIGHT/2.0f - User_Options.outHeight);
         OPPONENT_FRAME_TOPRIGHT_Y = OPPONENT_FRAME_TOPLEFT_Y;
         OPPONENT_FRAME_BOTTOMLEFT_X = OPPONENT_FRAME_TOPLEFT_X;
         OPPONENT_FRAME_BOTTOMRIGHT_X = OPPONENT_FRAME_TOPRIGHT_X;
-        OPPONENT_FRAME_BOTTOMLEFT_Y = OPPONENT_FRAME_TOPLEFT_Y + (float)frame.getHeight();
+        OPPONENT_FRAME_BOTTOMLEFT_Y = OPPONENT_FRAME_TOPLEFT_Y + (float)opponent.getHeight();
         OPPONENT_FRAME_BOTTOMRIGHT_Y = OPPONENT_FRAME_BOTTOMLEFT_Y;
         
-        canvas.drawBitmap(frame, OPPONENT_FRAME_TOPLEFT_X, OPPONENT_FRAME_TOPLEFT_Y, null);
+        canvas.drawBitmap(opponent, OPPONENT_FRAME_TOPLEFT_X, OPPONENT_FRAME_TOPLEFT_Y, null);
         brush.setStyle(Paint.Style.STROKE);
         brush.setColor(Color.argb(128, 255, 255, 255));
         brush.setStrokeWidth(10.0f);
-        canvas.drawRect(OPPONENT_FRAME_TOPLEFT_X + frame.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, 
+        canvas.drawRect(OPPONENT_FRAME_TOPLEFT_X + opponent.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, 
                 OPPONENT_FRAME_TOPLEFT_Y - 40.0f, 
-                OPPONENT_FRAME_TOPLEFT_X + frame.getWidth()/2.0f + HEALTH_BAR_SIZE/2.0f, 
+                OPPONENT_FRAME_TOPLEFT_X + opponent.getWidth()/2.0f + HEALTH_BAR_SIZE/2.0f, 
                 OPPONENT_FRAME_TOPRIGHT_Y - 30.0f, 
                 brush);
         brush.setStyle(Paint.Style.STROKE);
         brush.setColor(getHealthBarColor(50));
         brush.setStrokeWidth(10.0f);
-        canvas.drawRect(OPPONENT_FRAME_TOPLEFT_X + frame.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, 
+        canvas.drawRect(OPPONENT_FRAME_TOPLEFT_X + opponent.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, 
                 OPPONENT_FRAME_TOPLEFT_Y - 40.0f, 
-                OPPONENT_FRAME_TOPLEFT_X + frame.getWidth()/2.0f + HEALTH_BAR_SIZE/2.0F - 0.5f*HEALTH_BAR_SIZE, 
+                OPPONENT_FRAME_TOPLEFT_X + opponent.getWidth()/2.0f + HEALTH_BAR_SIZE/2.0F - 0.5f*HEALTH_BAR_SIZE, 
                 OPPONENT_FRAME_TOPRIGHT_Y - 30.0f, 
                 brush);
         brush.setColor(Color.argb(255, 255, 255, 255));
         brush.setStyle(Paint.Style.FILL);
         brush.setTextSize(25.0f);
-        canvas.drawText("HP", OPPONENT_FRAME_TOPLEFT_X + frame.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f - 50.0f, OPPONENT_FRAME_TOPLEFT_Y - 25.0f, brush);
+        canvas.drawText("HP", OPPONENT_FRAME_TOPLEFT_X + opponent.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f - 50.0f, OPPONENT_FRAME_TOPLEFT_Y - 25.0f, brush);
         brush.setTextSize(35.0f);
         
         if(pokemon.equals("nidorang")) temp = "Nidoran♀";
         else if(pokemon.equals("nidoranb")) temp = "Nidoran♂";
         else temp = pokemon.substring(0, 1).toUpperCase() + pokemon.substring(1) + "\tLVL???";
 
-        canvas.drawText(temp, OPPONENT_FRAME_TOPLEFT_X + frame.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, OPPONENT_FRAME_TOPLEFT_Y - 50.0f, brush);
+        canvas.drawText(temp, OPPONENT_FRAME_TOPLEFT_X + opponent.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, OPPONENT_FRAME_TOPLEFT_Y - 50.0f, brush);
         
-        USER_FRAME_TOPLEFT_X = 0.5f*(canvas.getWidth()/3.0f - options.outWidth/2.0f) - shift_x2;
-        USER_FRAME_TOPRIGHT_X = USER_FRAME_TOPLEFT_X + frame2.getWidth();
-        USER_FRAME_TOPLEFT_Y = 1.0f*(13*canvas.getHeight()/20.0f + LARGEST_HEIGHT/2.0f - options.outHeight);
+        USER_FRAME_TOPLEFT_X = 0.5f*(canvas.getWidth()/3.0f - Opponent_Options.outWidth/2.0f) - shift_x2;
+        USER_FRAME_TOPRIGHT_X = USER_FRAME_TOPLEFT_X + user.getWidth();
+        USER_FRAME_TOPLEFT_Y = 1.0f*(13*canvas.getHeight()/20.0f + LARGEST_HEIGHT/2.0f - Opponent_Options.outHeight);
         USER_FRAME_TOPRIGHT_Y = USER_FRAME_TOPLEFT_Y;
         USER_FRAME_BOTTOMLEFT_X = USER_FRAME_TOPLEFT_X;
         USER_FRAME_BOTTOMRIGHT_X = USER_FRAME_TOPRIGHT_X;
-        USER_FRAME_BOTTOMLEFT_Y = USER_FRAME_TOPLEFT_Y + frame2.getHeight();
+        USER_FRAME_BOTTOMLEFT_Y = USER_FRAME_TOPLEFT_Y + user.getHeight();
         USER_FRAME_BOTTOMRIGHT_Y = USER_FRAME_BOTTOMLEFT_Y;
         
-        canvas.drawBitmap(frame2, USER_FRAME_TOPLEFT_X, USER_FRAME_TOPLEFT_Y, null);
+        canvas.drawBitmap(user, USER_FRAME_TOPLEFT_X, USER_FRAME_TOPLEFT_Y, null);
         
         brush.setStyle(Paint.Style.STROKE);
         brush.setColor(Color.argb(128, 255, 255, 255));
         brush.setStrokeWidth(10.0f);
-        canvas.drawRect(USER_FRAME_TOPLEFT_X + frame2.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, 
+        canvas.drawRect(USER_FRAME_TOPLEFT_X + user.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, 
                 USER_FRAME_TOPLEFT_Y - 40.0f, 
-                USER_FRAME_TOPLEFT_X + frame2.getWidth()/2.0f + HEALTH_BAR_SIZE/2.0f, 
+                USER_FRAME_TOPLEFT_X + user.getWidth()/2.0f + HEALTH_BAR_SIZE/2.0f, 
                 USER_FRAME_TOPRIGHT_Y - 30.0f, 
                 brush);
         brush.setStyle(Paint.Style.STROKE);
         brush.setColor(getHealthBarColor(75));
         brush.setStrokeWidth(10.0f);
-        canvas.drawRect(USER_FRAME_TOPLEFT_X + frame2.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, 
+        canvas.drawRect(USER_FRAME_TOPLEFT_X + user.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, 
                 USER_FRAME_TOPLEFT_Y - 40.0f, 
-                USER_FRAME_TOPLEFT_X + frame2.getWidth()/2.0f + HEALTH_BAR_SIZE/2.0f - 0.25f*HEALTH_BAR_SIZE, 
+                USER_FRAME_TOPLEFT_X + user.getWidth()/2.0f + HEALTH_BAR_SIZE/2.0f - 0.25f*HEALTH_BAR_SIZE, 
                 USER_FRAME_TOPRIGHT_Y - 30.0f, 
                 brush);
         brush.setColor(Color.argb(255, 255, 255, 255));
         brush.setStyle(Paint.Style.FILL);
         brush.setTextSize(25.0f);
-        canvas.drawText("HP", USER_FRAME_TOPLEFT_X + frame2.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f - 50.0f, USER_FRAME_TOPLEFT_Y - 25.0f, brush);
+        canvas.drawText("HP", USER_FRAME_TOPLEFT_X + user.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f - 50.0f, USER_FRAME_TOPLEFT_Y - 25.0f, brush);
         brush.setTextSize(35.0f);
         
         if(pokemon.equals("nidorang")) temp = "Nidoran♀";
         else if(pokemon.equals("nidoranb")) temp = "Nidoran♂";
         else temp = pokemon.substring(0, 1).toUpperCase() + pokemon.substring(1) + "\tLVL???";
         
-        canvas.drawText(temp, USER_FRAME_TOPLEFT_X + frame2.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, USER_FRAME_TOPLEFT_Y - 50.0f, brush);
+        canvas.drawText(temp, USER_FRAME_TOPLEFT_X + user.getWidth()/2.0f - HEALTH_BAR_SIZE/2.0f, USER_FRAME_TOPLEFT_Y - 50.0f, brush);
         
         if(draw){
             orientation = "front";
@@ -180,17 +178,17 @@ public class Animated extends View {
             try {
                 temp = "sprites/" + pokemon + "/" + orientation + "/frame_";
                 i += 1;
-                stream = assets.open(temp + i + ".png");
+                stream = asset.open(temp + i + ".png");
                 decodeResizedBitmapFromAssets(LARGEST_WIDTH, LARGEST_HEIGHT);
             } catch (Exception var30) {
                 try {
                     i = 0;
-                    stream = assets.open("sprites/" + pokemon + "/" + orientation + "/frame_" + i + ".png");
+                    stream = asset.open("sprites/" + pokemon + "/" + orientation + "/frame_" + i + ".png");
                     decodeResizedBitmapFromAssets(LARGEST_WIDTH, LARGEST_HEIGHT);
                 } catch (Exception var29) {
                     try {
                        i = 1;
-                       stream = assets.open("sprites/" + pokemon + "/" + orientation + "/frame_" + i + ".png");
+                       stream = asset.open("sprites/" + pokemon + "/" + orientation + "/frame_" + i + ".png");
                        decodeResizedBitmapFromAssets(LARGEST_WIDTH, LARGEST_HEIGHT);
                     } catch (Exception var28) {
                         Battle.text.setText("Cannot find opponent sprite!");
@@ -204,17 +202,17 @@ public class Animated extends View {
             try {
                 temp = "sprites/" + pokemon + "/" + orientation + "/frame_";
                 j += 1;
-                stream = assets.open(temp + j + ".png");
+                stream = asset.open(temp + j + ".png");
                 decodeResizedBitmapFromAssets(LARGEST_WIDTH, LARGEST_HEIGHT);
             } catch (Exception var26) {
                 try {
                     j = 0;
-                    stream = assets.open("sprites/" + pokemon + "/" + orientation + "/frame_" + j + ".png");
+                    stream = asset.open("sprites/" + pokemon + "/" + orientation + "/frame_" + j + ".png");
                     decodeResizedBitmapFromAssets(LARGEST_WIDTH, LARGEST_HEIGHT);
                 } catch (Exception var25) {
                     try {
                         j = 1;
-                        stream = assets.open("sprites/" + pokemon + "/" + orientation + "/frame_" + j + ".png");
+                        stream = asset.open("sprites/" + pokemon + "/" + orientation + "/frame_" + j + ".png");
                         decodeResizedBitmapFromAssets(LARGEST_WIDTH, LARGEST_HEIGHT);
                     } catch (Exception var24) {
                         Battle.text.setText("Cannot find user sprite!");
@@ -239,8 +237,8 @@ public class Animated extends View {
 
     public Matrix placement(int layer){
         Matrix matrix = new Matrix();
-        if (layer == 1) matrix.setTranslate((width*2/3.0f - options2.outWidth/2.0f)*1.25f, ((height/4.0f + LARGEST_HEIGHT/2.0f) -  options2.outHeight)*1.0f);
-        else if (layer == 2) matrix.setTranslate((width/3.0f - options.outWidth/2.0f)*0.5f, (((height*2/3.0f) +  LARGEST_HEIGHT/2.0f) - options.outHeight)*1.0f);
+        if (layer == 1) matrix.setTranslate((width*2/3.0f - User_Options.outWidth/2.0f)*1.25f, ((height/4.0f + LARGEST_HEIGHT/2.0f) -  User_Options.outHeight)*1.0f);
+        else if (layer == 2) matrix.setTranslate((width/3.0f - Opponent_Options.outWidth/2.0f)*0.5f, (((height*2/3.0f) +  LARGEST_HEIGHT/2.0f) - Opponent_Options.outHeight)*1.0f);
         
         return matrix;
     }
@@ -261,56 +259,72 @@ public class Animated extends View {
 
     private void calculateDecSampleSize(int reqWidth, int reqHeight){
         int decSampleSize = 1;
-        if(options.outHeight > reqHeight || options.outWidth > reqWidth){
-            y = options.outHeight/2;
-            x = options.outWidth/2;
+        if(Opponent_Options.outHeight > reqHeight || Opponent_Options.outWidth > reqWidth){
+            y = Opponent_Options.outHeight/2;
+            x = Opponent_Options.outWidth/2;
             
             while(y/decSampleSize >= reqHeight && x/decSampleSize >= reqWidth) decSampleSize *= 2;            
         }
-        options.inSampleSize = decSampleSize;
+        Opponent_Options.inSampleSize = decSampleSize;
     }
 
     private void calculateIncSampleSize(int reqWidth, int reqHeight){
         int incSampleSize = 1;
-        if(options.outHeight < reqHeight || options.outWidth < reqWidth){
-            y = options.outHeight*2;
-            x = options.outWidth*2;
+        if(Opponent_Options.outHeight < reqHeight || Opponent_Options.outWidth < reqWidth){
+            y = Opponent_Options.outHeight*2;
+            x = Opponent_Options.outWidth*2;
             
             while(y*incSampleSize < reqHeight && x*incSampleSize < reqWidth) incSampleSize *= 2;            
         }
-        options.inDensity = 1;
-        options.inTargetDensity = 4;
-        options.inScaled = true;
+        Opponent_Options.inDensity = 1;
+        Opponent_Options.inTargetDensity = 4;
+        Opponent_Options.inScaled = true;
     }
 
     private void decodeResizedBitmapFromAssets(int reqWidth, int reqHeight){
         if(contender == 1){
-            options = new Options();
-//            options.inJustDecodeBounds = false;
-//            options.inDensity = 1;
-//            options.inTargetDensity = 4;
-//            options.inScaled = true;
-            frame = BitmapFactory.decodeStream(stream, null, options);
+            Opponent_Options = new Options();
+//            Opponent_Options.inJustDecodeBounds = false;
+//            Opponent_Options.inDensity = 1;
+//            Opponent_Options.inTargetDensity = 4;
+//            Opponent_Options.inScaled = true;
+            opponent = BitmapFactory.decodeStream(stream, null, Opponent_Options);
         } else {        
-            options2 = new Options();
-//            options2.inJustDecodeBounds = false;
-//            options2.inDensity = 1;
-//            options2.inTargetDensity = 5;
-//            options2.inScaled = true;
-            frame2 = BitmapFactory.decodeStream(stream, null, options2);
+            User_Options = new Options();
+//            User_Options.inJustDecodeBounds = false;
+//            User_Options.inDensity = 1;
+//            User_Options.inTargetDensity = 5;
+//            User_Options.inScaled = true;
+            user = BitmapFactory.decodeStream(stream, null, User_Options);
         }
     }
 
+    /* Method used for designing the UI, arranging elements throughout */
     private void setLargest(){
+        Options options;
+        
         try {
-            folders = assets.list("fol");
+            folders = asset.list("sprites");
             
             for(String folder : folders){
-                files = assets.list("sprites/" + folder + "/" + orientation);
-                options.inJustDecodeBounds = false;
-                frame = BitmapFactory.decodeStream(assets.open("sprites/" + folder + "/" + orientation + "/" + files[0]), null, options);
-                if(options.outWidth > LARGEST_WIDTH) LARGEST_WIDTH = options.outWidth;
-                if(options.outHeight > LARGEST_HEIGHT) LARGEST_HEIGHT = options.outHeight;                
+                files = asset.list("sprites/" + folder);
+                
+                for(String fileFolder : files){
+                    frames = asset.list("sprites/" + folder + "/" + fileFolder);
+                    
+                    for(String file : frames){
+                        options = new Options();
+                        opponent = BitmapFactory.decodeStream(asset.open("sprites/" + folder + "/" + fileFolder + "/" + file), null, options);
+                        if(options.outWidth > LARGEST_WIDTH){
+                            LARGEST_WIDTH = options.outWidth;
+                            Largest_Width_Details = "sprites/" + folder + "/" + fileFolder + "/" + file + " " + String.valueOf(LARGEST_WIDTH);
+                        }
+                        if(options.outHeight > LARGEST_HEIGHT){
+                            LARGEST_HEIGHT = options.outHeight;
+                            Largest_Height_Details = "sprites/" + folder + "/" + fileFolder + "/" + file + " " + String.valueOf(LARGEST_HEIGHT);
+                        }
+                    }
+                }
             }
         } catch (Exception e) {
         }
